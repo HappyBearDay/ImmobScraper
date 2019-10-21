63d1f8a65031d15cc40490002c78d844e68c64d9
Vincent F <vince.boba@gmail.com>
2019-10-21 11:39:35 -0700

    fix : leboncoin working, user-agent fix

--- scraper_leboncoin.py
+++ scraper_leboncoin.py
@@ -11,24 +11,25 @@ import math
 from VirtualScraper import Scraper
 from Parameters import url_leboncoin, page_add_leboncoin
 
-class LebonCoin_Scraper(Scraper):
 
+class LebonCoin_Scraper(Scraper):
 
     def find_json_in_scripts(self, script_list):
 
         for script_item in script_list:
             if 'window.__REDIAL_PROPS__' in script_item.text:
-                self.logger.debug(f"script_item window.__REDIAL_PROPS__ : {pformat(script_item.text)} ")
+                self.logger.debug(f"script_item window.__REDIAL_PROPS__ : {script_item.text} ")
 
-                string_data = script_item.text.split('=', 1)[1][4]
-                json_data = json.loads(string_data)
+                string_data = script_item.text.split('=', 1)[1]
+                self.logger.debug(f"string_data : {pformat(string_data)} ")
+
+                json_data = json.loads(string_data)[4]["data"]
                 self.logger.debug(f"Raw JSON : {pformat(json_data)} ")
 
                 return json_data
 
         logging.error("Non __REDIAL_PROPS__ on loaded Page")
         return None
-
 
     def get_appt_list(self, json_data):
         tmp_list_appt_1 = json_data['ads']
@@ -39,7 +40,6 @@ class LebonCoin_Scraper(Scraper):
         # list_appt = list(filter(lambda x: x['publicationId'] is not None, list(tmp_list_appt)))
 
         return tmp_list_appt
-
 
 
 if __name__ == '__main__':
